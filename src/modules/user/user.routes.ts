d34cec5614ac784c2ce7ab9684ef39b62e576587
Priyanshu <sidarsunita2@gmail.com>
2026-02-26 03:18:55 +0530

    booking schema added

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { validate } from "../../middlewares/validate.middleware";
+import { authMiddleware } from "../auth/auth.middleware";
 
 const router = Router();
+
+router.all("/user",authMiddleware);
 
 // router.post("/user", validate(getUserSchema), UserController.getUser);
 // router.put("/user", validate(updateUserSchema), UserController.updateUser);
